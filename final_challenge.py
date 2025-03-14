e5ef0d9cc1491031d7b01f9fb0ffe998010bc1fb
Axronl7 <Aaronliu657@gmail.com>
2025-03-14 10:10:52 -0400

    final challenge done

--- final_challenge.py
+++ final_challenge.py
@@ -1,2 +1,18 @@
 temperatures = ["Label", 32, 50, 77, 104]
-fahrenheit = {row[0]: temp * 5 for temp in temperatures}
+
+
+for temp in temperatures[1:]:
+
+    map(int, temperatures[1:])
+    celsius = map(lambda temp: (temp - 32) * 5.0 / 9.0, temperatures[1:])
+
+
+print(list(celsius))
+
+
+
+
+
+
+
+    
